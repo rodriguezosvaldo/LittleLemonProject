7ce497ae8809bed6f57d090f95850f30c6794111
Osvaldo <osvaldo.rguez911001@gmail.com>
2024-10-05 23:48:49 -0400

    Adding migrations

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -83,9 +83,16 @@ WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemonn',
+        'USER': 'root123',
+        'PASSWORD': 'root12345',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+          }
+     }
 }
 
 
